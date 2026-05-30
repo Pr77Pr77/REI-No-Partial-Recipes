8adfc8866d03bf3fce7b0cb0992ca9756d4fce0b
Pr77Pr77 <mtla.baur@gmail.com>
2026-05-30 21:47:33 +0200

    Using the more stable ResourceManager to retrieve the recipe JSONs instead of using ZipFile.
    This should fix issue #1

--- src/main/java/de/pr77pr77/rei/nopartialrecipes/RecipeJsonDumper.java
+++ src/main/java/de/pr77pr77/rei/nopartialrecipes/RecipeJsonDumper.java
@@ -4,67 +4,62 @@ import com.google.gson.*;
 import net.minecraft.recipe.*;
 import net.minecraft.registry.*;
 import net.minecraft.resource.*;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.zip.ZipFile;
-
-import static com.google.gson.JsonParser.parseReader;
 
 public final class RecipeJsonDumper {
     public static List<RecipeData> recipes;
 
-    public static class RecipeData{
-        Identifier id;
-        JsonElement json;
+    public static class RecipeData {
+        final Identifier id;
+        final JsonElement json;
 
-        RecipeData(Identifier id, JsonElement json){
+        RecipeData(Identifier id, JsonElement json) {
             this.id = id;
             this.json = json;
         }
     }
 
     public static void init() {
-        loadRecipes();
+        List<ResourcePack> packs = new ArrayList<>();
+
+        packs.add(VanillaDataPackProvider.createDefaultPack());
+
+        LifecycledResourceManagerImpl resourceManager = new LifecycledResourceManagerImpl(
+                ResourceType.SERVER_DATA,
+                packs
+        );
+
+        loadRecipes(resourceManager);
+        resourceManager.close();
     }
 
-    public static File getJar(Class<?> clazz) {
-        String path = clazz.getProtectionDomain()
-                .getCodeSource()
-                .getLocation()
-                .getPath();
-        return new File(path);
-    }
-
-    private static void loadRecipes() {
+    private static void loadRecipes(ResourceManager resourceManager) {
         recipes = new ArrayList<>();
-        try (ZipFile zip = new ZipFile(getJar(MinecraftServer.class))) {
-            zip.stream()
-                    .filter(e -> e.getName().startsWith("data/minecraft/recipe/") && e.getName().endsWith(".json"))
-                    .forEach(e -> {
-                        try (InputStream is = zip.getInputStream(e);
-                             InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
-                            JsonElement json = parseReader(reader);
 
-                            String path = e.getName();
+        Map<Identifier, Resource> resources = resourceManager.findResources(
+                "recipe",
+                id -> id.getNamespace().equals("minecraft") && id.getPath().endsWith(".json")
+        );
 
-                            int lastSlash = path.lastIndexOf('/');
-                            String filename = (lastSlash >= 0) ? path.substring(lastSlash + 1) : path;
+        for (Map.Entry<Identifier, Resource> entry : resources.entrySet()) {
+            try (InputStream is = entry.getValue().getInputStream();
+                 InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
 
-                            if (filename.endsWith(".json")) {
-                                filename = filename.substring(0, filename.length() - 5);
-                            }
+                JsonElement json = JsonParser.parseReader(reader);
 
-                            recipes.add(new RecipeData(Identifier.of("minecraft", filename), json));
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                        }
-                    });
-        } catch (IOException e) {
-            e.printStackTrace();
+                String path = entry.getKey().getPath();
+                String name = path
+                        .substring("recipe/".length())
+                        .replaceAll("\\.json$", "");
+
+                recipes.add(new RecipeData(Identifier.ofVanilla(name), json));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
